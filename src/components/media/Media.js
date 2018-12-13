40d56556f61b6f69f20ef569e25722b5e1f2cb0f
Neil Brown <nebrown@hof.co.uk>
2018-12-13 10:16:17 +0000

    updating layout

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Img from 'gatsby-image'
-import MediaLink from './mediaLink'
-import Styles from './media.module.scss'
+import React from "react";
+import Img from "gatsby-image";
+import MediaLink from "./mediaLink";
+import LayoutStyles from '../../layout.module.scss'
+import Styles from "./media.module.scss";
 
 const Media = props => {
   console.log(props);
   return (
-    <section className={Styles.media}>
-      <MediaLink title={props.title} />
-      <Img fluid={props.image.childImageSharp.fluid} />
+    <section className={LayoutStyles.layoutFlexRow}>
+      <section className={Styles.media}>
+        <MediaLink title={props.title} />
+        <Img fluid={props.image.childImageSharp.fluid} />
+      </section>
+      <section className={Styles.mediaContent}>
+        <h2>Some title</h2>
+        <p>some description</p>
+      </section>
     </section>
   );
 };
